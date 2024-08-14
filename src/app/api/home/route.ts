638839ec982ab21823748d275ff356ab273282e0
marcel <chukwuma.marcel@quickcheck.ng>
2024-08-14 02:24:56 +0100

    temp-fix

--- src/app/api/home/route.ts
+++ src/app/api/home/route.ts
@@ -3,6 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
 
 export async function GET(request: NextRequest) {
-    const skills = await prisma.skill.findMany({ orderBy: { name: 'asc' }});
+    // const skills = await prisma.skill.findMany({ orderBy: { name: 'asc' }});
+    const skills: { id: string }[] = [];
     return NextResponse.json({ skills });
 }
